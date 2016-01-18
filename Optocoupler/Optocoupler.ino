43ee439e9105a82a005b1dca27f875dd1b258146
kterf <edu.ribas.92@gmail.com>
2016-01-18 16:07:25 +0100

    Optocoupler shooting
    Optocoupler shooting: parameters and timings

--- Optocoupler/Optocoupler.ino
+++ Optocoupler/Optocoupler.ino
@@ -1,30 +1,80 @@
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
 
-  Most Arduinos have an on-board LED you can control. On the Uno and
-  Leonardo, it is attached to digital pin 13. If you're unsure what
-  pin the on-board LED is connected to on your Arduino model, check
-  the documentation at http://www.arduino.cc
+// printing string of characters and values
+#include <stdio.h>
+int l = 0;
 
-  This example code is in the public domain.
+// connections:
+int optoFocus = 11;
+int optoShutter = 12;
 
-  modified 8 May 2014
-  by Scott Fitzgerald
- */
+// shooting parameters:
+int totalPhotos = 10; // number of photos
+int counterPhotos = 0;
+long intervalTime = 5000; // (ms)
+long focusTime = 500; // (ms)
+long expoTime = 1000; // (ms)
+int FPS = 24;
+int state = 0;// 0: waiting, 1: focusing, 2: shooting
+unsigned long stateTime = intervalTime; // en quin moment ho assignare??
+boolean start = true;
 
-int pin = 13;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin 13 as an output.
-  pinMode(pin, OUTPUT);
+  // initialize digital pins 12 and 13 as outputs
+  pinMode(optoFocus, OUTPUT);
+  pinMode(optoShutter, OUTPUT);
+
+  digitalWrite(optoFocus, LOW);
+  digitalWrite(optoShutter, LOW);
+  
+  Serial.begin(9600);
+  Serial.println("initializing...\n");
+
+  char buffer [50];
+  l=sprintf(buffer, "N: %d\n\n", counterPhotos);
+  for(int i= 0; i<l; i++) 
+    Serial.print(buffer[i]);
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(pin, LOW);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);              // wait for a second
-  digitalWrite(pin,HIGH);    // turn the LED off by making the voltage LOW
-  delay(2000);              // wait for a second
+void loop(){
+  
+  if(start){
+
+    if(state == 0 && millis() > stateTime){ 
+      // start focusing
+      stateTime = millis() + focusTime;
+      state = 1;
+      digitalWrite(optoFocus, HIGH);
+      Serial.println("focusing...");
+    }
+
+    if(state == 1 && millis() > stateTime){ 
+      // start shooting
+      stateTime = millis() + expoTime;
+      state = 2;
+      digitalWrite(optoShutter, HIGH);
+      Serial.println("shooting...");
+    }
+
+    if(state == 2 && millis() > stateTime){
+      // stops focusing and shooting, starts waiting
+      stateTime = millis() + intervalTime;
+      state = 0;
+      counterPhotos++;
+      digitalWrite(optoFocus, LOW);
+      digitalWrite(optoShutter, LOW);
+      
+      char buffer [50];
+      l=sprintf(buffer, "N: %d\n\n", counterPhotos);
+      for(int i= 0; i<l; i++) 
+        Serial.print(buffer[i]);
+      
+      if(counterPhotos == totalPhotos)
+        start = false;
+    }
+
+  }
+
 }
