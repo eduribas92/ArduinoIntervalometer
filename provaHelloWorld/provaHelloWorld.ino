ffc6a13dd8b4d25eb5f8c9070967c15757c07248
kterf <edu.ribas.92@gmail.com>
2016-01-19 20:04:34 +0100

    Proves LCD Display

--- provaHelloWorld/provaHelloWorld.ino
+++ provaHelloWorld/provaHelloWorld.ino
@@ -54,6 +54,7 @@ void setup() {
 }
 
 void loop() {
+  /*
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
@@ -74,5 +75,6 @@ void loop() {
       lcd.print("_");
     }
   }
+  */
 }
 
