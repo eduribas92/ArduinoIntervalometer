3ceb0199da971668baf056893a9ec9ecaa42f6db
kterf <edu.ribas.92@gmail.com>
2016-01-20 20:24:52 +0100

    Improvements
    Code improvements, ideas,…

--- Optocoupler/Optocoupler.ino
+++ Optocoupler/Optocoupler.ino
@@ -8,14 +8,18 @@ int optoFocus = 11;
 int optoShutter = 12;
 
 // shooting parameters:
-int totalPhotos = 10; // number of photos
+int totalPhotos = 1; // number of photos
 int counterPhotos = 0;
 long intervalTime = 5000; // (ms)
-long focusTime = 500; // (ms)
-long expoTime = 1000; // (ms)
+long focusTime = 750; // (ms)
+long expoTime = 100; // (ms)
 int FPS = 24;
 int state = 0;// 0: waiting, 1: focusing, 2: shooting
-unsigned long stateTime = intervalTime; // en quin moment ho assignare??
+unsigned long stateTime = intervalTime; // en quin moment ho assignare?? 
+// si faig un time-lapse amb un gran interval time, trigara molt a començar!
+// fer stateTime = 10s? o startingDelay x2, o simliar, o directament = 0 i amb el startingDelay ja ho controlo
+
+int startingDelay = 5; // (s)
 boolean start = true;
 
 
@@ -30,6 +34,12 @@ void setup() {
   
   Serial.begin(9600);
   Serial.println("initializing...\n");
+  
+  while(startingDelay){
+    Serial.println(startingDelay);
+    startingDelay--;
+    delay(1000);
+  }
 
   char buffer [50];
   l=sprintf(buffer, "N: %d\n\n", counterPhotos);
@@ -71,8 +81,10 @@ void loop(){
       for(int i= 0; i<l; i++) 
         Serial.print(buffer[i]);
       
-      if(counterPhotos == totalPhotos)
+      if(counterPhotos == totalPhotos){
         start = false;
+        Serial.print("END\n");
+      }
     }
 
   }
