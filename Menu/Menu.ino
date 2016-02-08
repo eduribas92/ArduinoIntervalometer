c9c3d0de1a5d5d6ec33ed0d330c7c15055f663a3
kterf <edu.ribas.92@gmail.com>
2016-02-08 20:59:39 +0100

    Improvements of the menu

--- Menu/Menu.ino
+++ Menu/Menu.ino
@@ -1,17 +1,9 @@
 //------------BUTTONS-------------
 
-// printing string of characters and values
-#include <stdio.h>
-int l = 0;
-
-const int pinButton1 = 9;    
-const int pinButton2 = 10;
-const int pinButton3 = 11;
+const int pinButton1 = 9, pinButton2 = 10, pinButton3 = 11;
 const int pinLed =  13;   
 
-int stateButton1 = 0;       
-int stateButton2 = 0; 
-int stateButton3 = 0;
+int stateButton1 = 0, stateButton2 = 0, stateButton3 = 0;
 
 /*
 int previousStateButton1 = 1;         
@@ -29,14 +21,18 @@ const int precision = 150; // (ms)
 
 //------------MENU----------------
 
-int level = 0;
-int val = 0;
-int levelInc = 0;
+int levelInc = 0, value = 0, level = 0, selectedOption = 0, numOptions = 4;
+int levelOption[] = {2,2,2,2}; // number of levels for each menu
 
-String incomingString;
-int menuSelection = 0;
+//------------PARAMETERS----------
 
-int value[] = {0,0,0,0};
+// valueOption = [totalValueOption, valueOptionLevel1, valueOptionLevel2,...]
+int valueOption_NumPhotos[] = {0,0};
+int valueOption_IntTime[] = {0,0};
+int valueOption_Focus[] = {0,0};
+int valueOption_Expo[] = {0,0};
+// FICAR-HO TOT EN UN VECTOR DE VECTORS? valueOption = [valueOption1, valueOption2, ...] 
+// --> SWITCH MOLT FACIL, NOMES CAL SUMAR VALUE AMB COORDENADES valueOption[selectedOption][levelOption]
 
 //--------------------------------
 
@@ -51,7 +47,8 @@ void setup() {
 
   Serial.begin(9600);
   Serial.println("\ninitializing...");
-  printMenu(menuSelection,value[menuSelection]);
+  printMenu(selectedOption);
+  //printMenu(selectedOption,value[selectedOption]);
 }
 
 void loop() {
@@ -67,180 +64,105 @@ void loop() {
     // if it is, the buttonState is HIGH:
     if( buttonPressed == false && stateButton1 == HIGH ) {      
       digitalWrite(pinLed, HIGH);
-      val = 1;
-      buttonPressed == true;  
+      value = 1;
+      buttonPressed == true;  // QUÈ PASSA SI HO ESCRIC CORRECTAMENT???? DEIXARA D'AUMENTAR EL VALOR GRADUALMENT??? 
     } 
+    // PUC DEIXAR-HO DE LA FORMA SEGÜENT?
+    // if( buttonPressed == false && stateButton1 == HIGH ) val = 1;
 
     if( buttonPressed == false && stateButton2 == HIGH ) {     
       digitalWrite(pinLed, HIGH);
-      val = -1;
+      value = -1;
       buttonPressed == true;  
     } 
 
     if( buttonPressed == false && stateButton3 == HIGH ) {      
       digitalWrite(pinLed, HIGH);
       levelInc = +1;
-      /*Serial.print("Level: ");
-      Serial.println(level);*/
       buttonPressed == true; 
     } 
-    
+
+    // --> pregunta: ENTRA A AQUEST IF???
     if( stateButton1 == LOW && stateButton2 == LOW && stateButton3 == LOW ){
       digitalWrite(pinLed, LOW);
       buttonPressed = false;
     }
   }
 
-  //segons el "menu Seleccionat", level pot prendre mes o menys valors, i els paràmetres que es modifiquen son diferents
-  if( levelInc !=0 || val != 0 ){
-
-    switch(menuSelection) {
-
-      case 0: // 1. Number of photos:
-        
-        // if 'level' has changed, update it
-        if( levelInc != 0 ){
-          level = (level + levelInc) % 2;
-          levelInc = 0;
+  // segons el "menu Seleccionat", level pot prendre mes o menys valors
+  // if 'level' has changed, update it
+  if( levelInc != 0 ){
+    level = (level + levelInc) % levelOption[selectedOption];
+    levelInc = 0;
+  }
+  
+  if( value != 0 ){
+    if(level == 0){
+      selectedOption = ( selectedOption + numOptions + value ) % numOptions;
+      value = 0; 
+      printMenu(selectedOption);
+      //printMenu(selectedOption,value[selectedOption]);
+    }else{
+      // segons menu i level, canvia els paràmetres adequats
+      switch(selectedOption) {
+        case 0: // 1. Number of photos:
+          valueOption_NumPhotos[level] += value;
+          value = 0;
+          valueOption_NumPhotos[0] = valueOption_NumPhotos[1];
+          Serial.print("\t");
+          Serial.println(valueOption_NumPhotos[0]);
+          break;
+        case 1: // 2. Interval time:
+          valueOption_IntTime[level] += value;
+          value = 0;
+          valueOption_IntTime[0] = valueOption_IntTime[1];
+          Serial.print("\t");
+          Serial.println(valueOption_IntTime[0]);
+          break;
+        case 2: // 3. Focus adjust:
+          valueOption_Focus[level] += value;
+          value = 0;
+          valueOption_Focus[0] = valueOption_Focus[1];
+          Serial.print("\t");
+          Serial.println(valueOption_NumPhotos[0]);
+          break;
+        case 3: // 4. Exposure adjust:
+          valueOption_Expo[level] += value;
+          value = 0;
+          valueOption_Expo[0] = valueOption_Expo[1];
+          Serial.print("\t");
+          Serial.println(valueOption_Expo[0]);
+          break;
+        default: 
+          Serial.println("Undefined menu!");
+          break;
         }
-
-        // if 'val' has changed, update depending on 'level'
-        if( val != 0 ){
-          switch(level){
-            case 0:
-              menuSelection = ( menuSelection + 4 + val ) % 4;
-              val = 0; 
-              printMenu(menuSelection,value[menuSelection]);  
-              break;
-              
-            case 1:
-              value[menuSelection] += val;
-              val = 0;
-              Serial.print("\t");
-              Serial.println(value[menuSelection]);
-              break;
-          }
-        }
-
-        break;
-              
-      case 1: // 2. Interval time:
-
-        // if 'level' has changed, update it
-        if( levelInc != 0 ){
-          level = (level + levelInc) % 2;
-          levelInc = 0;
-        }
-
-        // if 'val' has changed, update depending on 'level'
-        if( val != 0 ){
-          switch(level){
-            case 0:
-              menuSelection = ( menuSelection + 4 + val ) % 4;
-              val = 0; 
-              printMenu(menuSelection,value[menuSelection]);  
-              break;
-              
-            case 1:
-              value[menuSelection] += val;
-              val = 0;
-              Serial.print("\t");
-              Serial.println(value[menuSelection]);
-              break;
-          }
-        }
-
-        break;
-              
-      case 2: // 3. Focus adjust:
-
-        // if 'level' has changed, update it
-        if( levelInc != 0 ){
-          level = (level + levelInc) % 2;
-          levelInc = 0;
-        }
-
-        // if 'val' has changed, update depending on 'level'
-        if( val != 0 ){
-          switch(level){
-            case 0:
-              menuSelection = ( menuSelection + 4 + val ) % 4;
-              val = 0; 
-              printMenu(menuSelection,value[menuSelection]);  
-              break;
-              
-            case 1:
-              value[menuSelection] += val;
-              val = 0;
-              Serial.print("\t");
-              Serial.println(value[menuSelection]);
-              break;
-          }
-        }
-
-        break;
-              
-      case 3: // 4. Exposure adjust:
-        
-        // if 'level' has changed, update it
-        if( levelInc != 0 ){
-          level = (level + levelInc) % 2;
-          levelInc = 0;
-        }
-
-        // if 'val' has changed, update depending on 'level'
-        if( val != 0 ){
-          switch(level){
-            case 0:
-              menuSelection = ( menuSelection + 4 + val ) % 4;
-              val = 0; 
-              printMenu(menuSelection,value[menuSelection]);  
-              break;
-              
-            case 1:
-              value[menuSelection] += val;
-              val = 0;
-              Serial.print("\t");
-              Serial.println(value[menuSelection]);
-              break;
-          }
-        }
-        
-        break;
-              
-      default: 
-        Serial.println("Undefined menu!");
-        break;
-
     }
+
+    
   }
 }
 
-
-void printMenu( int menuSelection, int value ){
-
+void printMenu(int selectedOption){
+//void printMenu( int selectedOption, int value ){
   Serial.print("\n");
-
-  switch(menuSelection) {
-
+  switch(selectedOption) {
     case 0:
       Serial.print("1. Number of photos: ");
-      break;
-              
+      break;              
     case 1:
       Serial.print("2. Interval time: ");
-      break;
-              
+      break;              
     case 2:
       Serial.print("3. Focus adjust: ");
-      break;
-              
+      break;              
     case 3:
       Serial.print("4. Exposure adjust: ");
       break;
+    default: 
+      Serial.println("Undefined menu!");
+      break;
   }
-  
-  Serial.println(value);
-
+  //Serial.println(value);
   return;
 }
