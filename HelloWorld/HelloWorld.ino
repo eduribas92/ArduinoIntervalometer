d113540472899186130f3e7c2d4490b705a5013a
kterf <edu.ribas.92@gmail.com>
2016-02-10 03:09:16 +0100

    lcd parameters

--- HelloWorld/HelloWorld.ino
+++ HelloWorld/HelloWorld.ino
@@ -41,7 +41,7 @@
 #include <LiquidCrystal.h>
 
 // initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 6, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   // set up the LCD's number of columns and rows:
